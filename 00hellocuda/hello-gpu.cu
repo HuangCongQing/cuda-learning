f800329c86d9e4f1d810810b1cf85424eebfd5d5
黄重庆 <1756260160@qq.com>
2022-02-02 21:25:35 +0800

    gpu配置 《block 线程数》

--- 00hellocuda/hello-gpu.cu
+++ 00hellocuda/hello-gpu.cu
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2022-02-02 20:13:18
- * @LastEditTime: 2022-02-02 20:54:06
+ * @LastEditTime: 2022-02-02 21:18:00
  * @FilePath: /cuda-learning/00hellocuda/hello-gpu.cu
  */
 #include <stdio.h>
@@ -14,12 +14,15 @@ void cpu(){
 }
 // global 将在gpu上运行并可全局调用
 __global__ void gpu(){
-    printf("hello gpu\n");
+    // 只希望第一个block的第一个线程去打印
+    if(blockIdx.x==0&&threadIdx.x==0){
+        printf("hello gpu\n");
+    }
 }
 
 
 int main(){
     cpu();
-    gpu<<<1,1>>>();  // gpu配置 《线程 线程块个数》
+    gpu<<<2,3>>>();  // gpu配置 《block 线程数》
     cudaDeviceSynchronize();
 }
