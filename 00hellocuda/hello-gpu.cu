bf140b45e0415aecf0097f2bc122109f4cacad00
黄重庆 <1756260160@qq.com>
2022-02-02 20:54:43 +0800

    add: ./hello-gpu

--- 00hellocuda/hello-gpu.cu
+++ 00hellocuda/hello-gpu.cu
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2022-02-02 20:13:18
- * @LastEditTime: 2022-02-02 20:39:35
+ * @LastEditTime: 2022-02-02 20:54:06
  * @FilePath: /cuda-learning/00hellocuda/hello-gpu.cu
  */
 #include <stdio.h>
@@ -12,7 +12,7 @@
 void cpu(){
     printf("hello cpu\n");
 }
-// global
+// global 将在gpu上运行并可全局调用
 __global__ void gpu(){
     printf("hello gpu\n");
 }
@@ -20,6 +20,6 @@ __global__ void gpu(){
 
 int main(){
     cpu();
-    gpu<<<1,1>>>();  // gpu配置
+    gpu<<<1,1>>>();  // gpu配置 《线程 线程块个数》
     cudaDeviceSynchronize();
 }
