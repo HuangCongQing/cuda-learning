1c9da967b6af78c1e08eb20db7743d3c72d7edd6
黄重庆 <1756260160@qq.com>
2022-02-03 10:26:10 +0800

    add

--- 00hellocuda/hello-gpu.cu
+++ 00hellocuda/hello-gpu.cu
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2022-02-02 20:13:18
- * @LastEditTime: 2022-02-02 21:18:00
+ * @LastEditTime: 2022-02-03 10:19:26
  * @FilePath: /cuda-learning/00hellocuda/hello-gpu.cu
  */
 #include <stdio.h>
@@ -14,6 +14,8 @@ void cpu(){
 }
 // global 将在gpu上运行并可全局调用
 __global__ void gpu(){
+    // 通过一个表达式区分不同线程
+    // blockIdx.x * blockDim.x + threadIdx.x; 
     // 只希望第一个block的第一个线程去打印
     if(blockIdx.x==0&&threadIdx.x==0){
         printf("hello gpu\n");
