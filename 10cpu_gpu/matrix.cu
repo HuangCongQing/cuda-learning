72da5f4628dcd729066b6779cbd5b0afa0b0c9c1
黄重庆 <1756260160@qq.com>
2022-02-03 20:56:19 +0800

    add: 内存分配

---
+++ 10cpu_gpu/matrix.cu
@@ -0,0 +1,75 @@
+/* 
+基于metirx修改
+*/
+#include <stdio.h>
+
+#define N 64 // 定义
+
+// Kernal函数
+__global__ void gpu(int *a, int *b, int *c_gpu){
+    int r = blockDim.x * blockIdx.x + threadIdx.x;
+    int c = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if(r < N && c < N){
+        c_gpu[r * N + c] = a[r * N + c] + b[r * N +c];
+    }
+    // printf("hello gpu\n");
+}
+
+// cpu
+void cpu(int *a, int *b, int *c_cpu){
+    for(int r = 0;r < N; r++){
+        for (int c = 0;c < N; c++){
+            c_cpu[r * N +c]  = a[r * N +c] + b[r * N + c];
+        }
+    }
+    printf("hello cpu\n");
+}
+
+
+// 验证
+bool check(int *a, int *b, int *c_cpu, int * c_gpu){
+    for(int r = 0;r < N; r++){
+        for(int c = 0;c < N; c++){
+            if(c_cpu[r * N +c] != c_gpu[r * N + c]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    int *a, *b, *c_cpu, *c_gpu;
+    size_t size = N * N * sizeof(int);
+
+    cudaMallocManaged(&a, size);
+    cudaMallocManaged(&b, size);
+    cudaMallocManaged(&c_cpu, size);
+    cudaMallocManaged(&c_gpu, size);
+    // 1 初始化
+    for(int r=0;r<N;r++){
+        for(int c=0;c<N;c++){
+            a[r * N + c]  = r;
+            b[r * N + c]  = c;
+            c_cpu[r * N + c]  = 0;
+            c_gpu[r * N + c]  = 0;
+        }
+    }
+    // 2 定义threads
+    dim3 threads(16, 16, 1); // 1维写成3维
+    dim3 blocks((N + threads.x - 1) / threads.x, (N + threads.y - 1) / threads.y, 1 );
+    // 调用
+    gpu<<<blocks, threads>>>(a, b, c_gpu);
+    cudaDeviceSynchronize();
+
+    cpu(a, b, c_cpu);
+    check(a, b, c_cpu, c_gpu) ? printf("ok") : printf("error");
+
+    // end: 释放内存
+    cudaFree(a); // 提前写，防止忘记释放内存
+    cudaFree(b); 
+    cudaFree(c_cpu); 
+    cudaFree(c_gpu); 
+    return 0;
+}
