fe6f36bf9b19ae018cfd009301d1249060a2576b
hcq <1756260160@qq.com>
2022-05-02 14:18:09 +0800

    add: 内存分配

--- 03malloc/malloc.cu
+++ 03malloc/malloc.cu
@@ -1,4 +1,8 @@
 
+// 分配空间函数不能放在核函数里面,放在外面. 
+// cudaMallocManaged(&a, size); // 分配内存，既可以被cpu使用也可以被gpu使用
+// cudaFree(a);  // 释放内存
+
 #include <stdio.h>
 #include <stdlib.h> // cpu的malloc函数
 
@@ -8,9 +12,12 @@ void cpu(int *a , int N){
     }
     printf("hello cpu\n");
 }
+
+
+// 函数定义:定义一个数组,把数组内的值放大两倍.
 // global 将在gpu上运行并可全局调用
 __global__ void gpu(int *a, int N){
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x; // 类似for循环了!
     if(i<N){
         a[i]*=2; // 放大2倍
     }
@@ -26,17 +33,17 @@ bool check(int *a, int N){
 
 
 int main(){
-    const int N = 2 << 5; //二进制左移运算符。
+    const int N = 2 << 5; //二进制左移运算符。2的5次方 多少个数
     size_t size = N*sizeof(int);
     int *a; //取指针的地址&a
-    cudaMallocManaged(&a, size); // 既可以被cpu使用也可以被gpu使用
+    cudaMallocManaged(&a, size); //  给a分配的内存.  cudaMallocManaged既可以被cpu使用也可以被gpu使用
+    // cpu
     cpu(a, N);
-    
     // gpu
-    size_t threads = 256;
-    size_t blocks = (N + threads  -1)/threads; // 算法竞赛向上取整  ceil也可
-    gpu<<<blocks, threads>>>(a, N); // 每一个数都拥有一个线程
-    cudaDeviceSynchronize();
+    size_t threads = 256; // 定义线程数量
+    size_t blocks = (N + threads  -1)/threads; //blocks 希望每一个数都拥有一个线程  | 算法竞赛向上取整,ceil也可
+    gpu<<<blocks, threads>>>(a, N); // 
+    cudaDeviceSynchronize(); // 同步一下,不然报错!
 
     check(a, N)?printf("Ok") : printf("Sorry");
     cudaFree(a);
