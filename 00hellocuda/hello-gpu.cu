4bf90737bdb444c43830933dc3d4229ed4b558e6
chongqinghuang <chongqinghuang@autox.ai>
2023-03-15 14:52:10 +0800

    Tips: 不用for循环 O(n)的算法直接变成O(1)

--- 00hellocuda/hello-gpu.cu
+++ 00hellocuda/hello-gpu.cu
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2022-02-02 20:13:18
- * @LastEditTime: 2022-05-01 13:08:44
+ * @LastEditTime: 2023-03-15 14:43:48
  * @FilePath: /cuda-learning/00hellocuda/hello-gpu.cu
  */
 #include <stdio.h>
@@ -27,6 +27,6 @@ __global__ void gpu(){
 
 int main(){
     cpu();
-    gpu<<<2,3>>>();  // gpu配置 《block 线程数》
+    gpu<<<2,3>>>();  // gpu配置 《block数 线程数》 不用for循环 O(n)的算法直接变成O(1)
     cudaDeviceSynchronize();
 }
